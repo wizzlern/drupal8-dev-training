a529900f0db9656374f69bc32e30aa36b2e2a91a
Erik Stielstra <info@erikstielstra.nl>
2014-08-18 22:59:19 +0200

    Initial commit Wizzlern Pegi module.

---
+++ wizzlern_pegi/src/Controller/WizzlernPegiController.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\wizzlern_pegi\Controller\WizzlernPegiController.
+ */
+
+namespace Drupal\wizzlern_pegi\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Entity\Query\QueryFactory;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Returns responses for Wizzlern Pegi module routes.
+ */
+class WizzlernPegiController extends ControllerBase {
+
+  /**
+   * The entity manager
+   *
+   * @var EntityManagerInterface
+   */
+  protected $entityManager;
+
+  /**
+   * The entity query manager
+   *
+   * @var QueryFactory
+   */
+  protected $entityQuery;
+
+  /**
+   * @todo
+   *
+   * @param EntityManagerInterface $entity_manager
+   * @param QueryFactory $entity_query
+   */
+  public function __construct(EntityManagerInterface $entity_manager, QueryFactory $entity_query) {
+
+    $this->entityManager = $entity_manager;
+    $this->entityQuery = $entity_query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+
+    return new static(
+      $container->get('entity.manager'),
+      $container->get('entity.query')
+    );
+  }
+
+  /**
+   * Controller content callback: All games.
+   *
+   * @return array
+   *   Render array of page output.
+   */
+  public function AllGames() {
+    $items = array();
+
+    $nids = $this->entityQuery->get('node')
+      ->condition('type', 'game')
+      ->condition('status', 1)
+      ->sort('created')
+      ->pager(5)
+      ->execute();
+    $nodes = $this->entityManager->getStorage('node')->loadMultiple($nids);
+
+    foreach ($nodes as $nid => $node) {
+      $items[$nid] = $this->entityManager->getViewBuilder('node')->view($node, 'teaser');
+    }
+    $build['games'] = array(
+      '#theme' => 'item_list',
+      '#items' => $items,
+    );
+    $build['pager'] = array('#theme' => 'pager');
+
+    return $build;
+  }
+
+}
---
+++ wizzlern_pegi/src/Plugin/Block/NewGames.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\wizzlern_pegi\Plugin\Block\NewGames.
+ */
+
+namespace Drupal\wizzlern_pegi\Plugin\Block;
+
+use Drupal\block\Annotation\Block;
+use Drupal\block\BlockBase;
+use Drupal\Core\Annotation\Translation;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Entity\Query\QueryFactory;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+
+/**
+ * Provides a recent games block.
+ *
+ * @Block(
+ *   id = "wizzlern_pegi_new_games",
+ *   subject = @Translation("New games"),
+ *   admin_label = @Translation("New games")
+ * )
+ */
+class NewGames extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity manager
+   *
+   * @var EntityManagerInterface
+   */
+  protected $entityManager;
+
+  /**
+   * The entity query manager
+   *
+   * @var QueryFactory
+   */
+  protected $entityQuery;
+
+  /**
+   * @todo
+   *
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   * @param EntityManagerInterface $entity_manager
+   * @param QueryFactory $entity_query
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityManagerInterface $entity_manager, QueryFactory $entity_query) {
+
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityManager = $entity_manager;
+    $this->entityQuery = $entity_query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+
+    return new static($configuration, $plugin_id, $plugin_definition, $container->get('entity.manager'), $container->get('entity.query'));
+  }
+
+  /**
+   * Overrides \Drupal\block\BlockBase::defaultConfiguration().
+   */
+  public function defaultConfiguration() {
+
+    return array(
+//      'label' => t('Games'),
+      'max_items' => 5,
+    );
+  }
+
+  /**
+   * Overrides \Drupal\block\BlockBase::blockForm().
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+
+    $form['max_items'] = array(
+      '#type' => 'number',
+      '#title' => t('Number of games'),
+      '#size' => 15,
+      '#description' => t('The maximum number of games to show.'),
+      '#default_value' => $this->configuration['max_items'],
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+
+    $this->configuration['max_items'] = $form_state->getValue('max_items');
+  }
+
+  /**
+   * Implements \Drupal\block\BlockBase::blockBuild().
+   */
+  public function build() {
+    $items = array();
+    $max = $this->configuration['max_items'];
+
+    // @todo Replace 'game' content type by configurable content types.
+    $nids = $this->entityQuery->get('node')
+      ->condition('type', 'game')
+      ->condition('status', 1)
+      ->range(0, $max)
+      ->sort('created', 'DESC')
+      ->execute();
+    $nodes = $this->entityManager->getStorage('node')->loadMultiple($nids);
+
+    foreach ($nodes as $node) {
+      $items[] = l($node->title->value, 'node/' . $node->id());
+    }
+
+    return array(
+      '#theme' => 'item_list',
+      '#items' => $items,
+    );
+
+  }
+}
