1946d6814c19a6f401695a8a04eedcd8003b78ca
Erik Stielstra <info@erikstielstra.nl>
2015-12-18 13:42:25 +0100

    Pegi reference field formatter: Remove deprecated methods.

--- wizzlern_pegi/src/Plugin/Field/FieldFormatter/PegiReferenceLinkFormatter.php
+++ wizzlern_pegi/src/Plugin/Field/FieldFormatter/PegiReferenceLinkFormatter.php
@@ -10,7 +10,6 @@ namespace Drupal\wizzlern_pegi\Plugin\Field\FieldFormatter;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Field\Plugin\Field\FieldFormatter\EntityReferenceFormatterBase;
-use Drupal\Core\Url;
 
 /**
  * Plugin implementation of the 'pegi reference label' formatter.
@@ -76,40 +75,46 @@ class PegiReferenceLinkFormatter extends EntityReferenceFormatterBase {
   /**
    * {@inheritdoc}
    */
-  public function viewElements(FieldItemListInterface $items) {
+  public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = array();
 
-    foreach ($this->getEntitiesToView($items) as $delta => $entity) {
+    foreach ($this->getEntitiesToView($items, $langcode) as $delta => $entity) {
       /** @var \Drupal\image\Plugin\Field\FieldType\ImageItem $image */
       // We assume that only one image is used.
       $item = $entity->{WIZZLERN_PEGI_FIELD_PEGI_LABEL}[0];
-      $url = $entity->url();
-      $image_style_setting = $this->getSetting('image_style');
+      $image_style = $this->getSetting('image_style');
 
       // Collect cache tags to be added for each item in the field.
       $cache_tags = array();
       if (!empty($image_style_setting)) {
-        $image_style = entity_load('image_style', $image_style_setting);
-        $cache_tags = $image_style->getCacheTags();
+        /** @var \Drupal\image\Entity\ImageStyle $image_style_entity */
+        // @todo Use DI for this service.
+        $image_style_entity = \Drupal::getContainer()
+          ->get('entity_type.manager')
+          ->getStorage('image_style')
+          ->load($image_style);
+        $cache_tags = $image_style_entity->getCacheTags();
       }
 
-      $image = array(
-        '#theme' => 'image_formatter',
-        '#item' => $item,
-        '#image_style' => $image_style_setting,
-        '#cache' => array(
-          'tags' => $cache_tags,
-        ),
-      );
+      if (isset($image_style)) {
+        $image = array(
+          '#theme' => 'image_formatter',
+          '#item' => $item,
+          '#image_style' => $this->getSetting('image_style'),
+          '#cache' => array(
+            'tags' => $cache_tags,
+          ),
+        );
 
-      $elements[] = array(
-        '#type' => 'inline_template',
-        '#template' => '{{ image }}<span class="pegi-link-all">{{ link }}</span>',
-        '#context' => array(
-          'image' => $image,
-          'link' => $entity->link($this->t('All games with this rating')),
-        ),
-      );
+        $elements[] = array(
+          '#type' => 'inline_template',
+          '#template' => '{{ image }}<span class="pegi-link-all">{{ link }}</span>',
+          '#context' => array(
+            'image' => $image,
+            'link' => $entity->toLink($this->t('All games with this rating')),
+          ),
+        );
+      }
     }
 
     return $elements;
