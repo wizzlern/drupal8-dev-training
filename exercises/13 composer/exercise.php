2ee9c46ca78209059fb771f495e6a72d364b5dfb
Erik Stielstra <info@erikstielstra.nl>
2016-01-09 16:40:38 +0100

    Update composer exercise.

--- exercises/13 composer/exercise.php
+++ exercises/13 composer/exercise.php
@@ -9,10 +9,9 @@
 // Use composer to load the SimpleHtmlDom library.
 // - Find SimpleHtmlDom library on internet.
 // - Check at packagist.org if this library is available
-// - Create a composer.json file and include the library.
-// - Run composer to install the library.
-
-// ==== Step 2 ====
-// Autoload the included libraries.
-// - Add an include statement to the module to include Composer's autoloader.
-// - Check if the library gets loaded.
+// - Add a composer.json file in the module directory. Include at least 'name'
+//   and 'required'. See https://packagist.org/ for examples.
+// - Download and install Composer Manager module (no need to enable).
+// - Read Composer Manager's README.txt and follow the instructions.
+// - Check that the drupal root /vendor directory now contains the SimpleHtmlDom
+//   library
