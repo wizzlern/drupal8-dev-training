644fc04aec2c415b9b455fe7001bd17d0a2608ba
Erik Stielstra <info@erikstielstra.nl>
2016-11-12 11:27:19 +0100

    Change composer exercise

--- exercises/13 composer/exercise.php
+++ exercises/13 composer/exercise.php
@@ -11,7 +11,7 @@
 // - Check at packagist.org if this library is available
 // - Add a composer.json file in the module directory. Include at least 'name'
 //   and 'required'. See https://packagist.org/ for examples.
-// - Download and install Composer Manager module (no need to enable).
-// - Read Composer Manager's README.txt and follow the instructions.
-// - Check that the drupal root /vendor directory now contains the SimpleHtmlDom
-//   library
+// - Install the library using "composer require ...". If you have installed
+//   Drupal with console, run composer require from the project root directory.
+//   Otherwise run composer require from the module directory.
+// - Check that the vendor directory now contains the SimpleHtmlDom library.
