36ae998a58f6f7493a08162131370f4076bcd6b1
Erik Stielstra <info@erikstielstra.nl>
2016-12-01 15:24:26 +0100

    Change the order of 03 module exercises

--- exercises/03.1 pegi module/exercise.php
+++ exercises/03.1 pegi module/exercise.php
@@ -6,13 +6,13 @@
  */
 
 // ==== Step 1 ====
-// Install the Wizzlern Pegi module
-// - Copy the wizzlern_pegi folder to your project's module/custom folder.
-// - Enable the module.
-
-// ==== Step 2 ====
 // Create the Games content type and Pegi vocabulary.
 // - Copy the wizzlern_pegi_config folder to your project's module/custom folder.
 //   This module contains the configuration of the Game Review content type and
 //   Pegi vocabulary that are required for the course exercises.
+// - Enable the module.
+
+// ==== Step 2 ====
+// Install the Wizzlern Pegi module
+// - Copy the wizzlern_pegi folder to your project's module/custom folder.
 // - Enable the module.
